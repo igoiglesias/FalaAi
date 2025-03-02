5eeea1196695e6a83c7eb96db3c731142ac13b01
Igor Iglesias <contato@igoriglesias.com>
2025-03-02 11:53:41 -0300

    Valida token nas requisicoes autenticadas

--- app/services/auth.py
+++ app/services/auth.py
@@ -3,7 +3,7 @@ import jwt
 from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse, RedirectResponse
 
-from ..config.settings import Settings
+from ..config.settings import settings
 
 
 class Auth:
@@ -17,10 +17,10 @@ class Auth:
             token = self.generate_token({"sub": usuario})
             response = JSONResponse(content={"message": "Login realizado com sucesso!"})
             response.set_cookie(
-                key=Settings.TOKEN_KEY,
+                key=settings.TOKEN_KEY,
                 value=token,
-                httponly=Settings.TOKEN_HTTPONLY,
-                max_age=Settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+                httponly=settings.TOKEN_HTTPONLY,
+                max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
                 samesite="Strict"
             )
             
@@ -28,19 +28,27 @@ class Auth:
         return JSONResponse(status_code=401, content={"message": "Credenciais inválidas"})
 
     def generate_token(self, data: dict) -> str:
-        expire = datetime.now() + timedelta(minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         data = {
             **data,
             "exp": expire
         }
-        return jwt.encode(data, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM)
+        return jwt.encode(data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    def validate_token(self, token) -> bool:
+        try:
+            if jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]):
+                return True
+            return False
+        except:
+            return False
 
     def is_auth(self):
         def decorator(fn):
             @functools.wraps(fn)
             async def wrapper(request, *args, **kwargs):
-                token = request.cookies.get(Settings.TOKEN_KEY)
-                if not token:
+                token = request.cookies.get(settings.TOKEN_KEY)
+                if not token or not self.validate_token(token):
                     return RedirectResponse('login', status_code=307)
                 return await fn(request, *args, **kwargs)
             return wrapper
